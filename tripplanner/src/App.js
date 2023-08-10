092e674b51a5be53073f24b6fae365b8b94616aa
zukardex <zenzenram@gmail.com>
2023-08-10 23:12:11 +0530

    nj version
    Co-Authored-By: Nino Jagadish <96694708+NJ-NJ@users.noreply.github.com>

---
+++ tripplanner/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import map from './components/map.js'
+
+function App() {
+  return (
+    <div className="App">
+      <map/>
+    </div>
+  );
+}
+
+export default App;
